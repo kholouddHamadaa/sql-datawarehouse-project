9872d8ac4d3f91f796a5ff3c7a0a498820c345a6
Kholoud Hamada Ahmed <kholoudh905@gmail.com>
2026-04-08 08:20:13 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -147,7 +147,7 @@ BEGIN
 		CASE WHEN TRIM(cntry) ='DE' THEN 'Germany'
 		WHEN TRIM(cntry) IN ('US' , 'USA') THEN 'United States'
 		WHEN TRIM(cntry) =' ' OR cntry IS NULL THEN 'n/a'
-		ELSE cntry
+		ELSE TRIM(cntry)
 		END AS cntry
 		FROM bronze.erp_loc_a101
 
